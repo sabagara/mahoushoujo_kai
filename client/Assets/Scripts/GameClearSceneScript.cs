ad54b10ebf8e285bc9a75ab31fac7dcde4389a64
sabagara <sabagara@gertrude.local>
2019-01-27 13:21:48 +0900

    エンディング対応

--- client/Assets/Scripts/GameClearSceneScript.cs
+++ client/Assets/Scripts/GameClearSceneScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameClearSceneScript : MonoBehaviour
 {
@@ -11,7 +12,19 @@ public class GameClearSceneScript : MonoBehaviour
     {
         scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
         SoundController.Instance.playBgm(SoundController.SOUND.BGM_GAME_OVER);
-        Debug.Log("PillpowNum:" + scoreManager.getPillowNum());
+
+        if (scoreManager.getPillowNum() == 0)
+        {
+            GameObject.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/clear0");
+        }
+        else if (scoreManager.getPillowNum() == 1)
+        {
+            GameObject.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/clear1");
+        }
+        else
+        {
+            GameObject.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/clear2");
+        }
     }
     void Update()
     {
--- client/Assets/Scripts/Goal.cs
+++ client/Assets/Scripts/Goal.cs
@@ -6,9 +6,16 @@ public class Goal : MonoBehaviour
 {
     private GameMain gameMain;
 
+    private float[] futonHomePos = { -3.6f, -1.3f, 1.3f, 3.6f };
+
     private void Start()
     {
         gameMain = GameObject.Find("GameMain").GetComponent<GameMain>();
+
+        Vector3 goalPosition = gameObject.transform.position;
+        int idx = Random.Range(0, 4);
+        goalPosition.x = futonHomePos[idx];
+        gameObject.transform.position = goalPosition;
     }
 
     void OnTriggerEnter(Collider col)
